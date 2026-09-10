e3bc6643d01941ac91c57f589f7886e110cd3f3f
Manikanta H <67575982+manikanta-h-java-backend-developer@users.noreply.github.com>
2026-09-10 09:02:54 +0530

    Initial setup files

---
+++ DatabaseMigrationApplication.java
@@ -0,0 +1,12 @@
+package com.purnesh.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMigrationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DatabaseMigrationApplication.class, args);
+  }
+}
